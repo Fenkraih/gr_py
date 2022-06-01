fef2c36108c8af297d4f9b67e1495da6ad39ba03
Altin <alsh03@dfki.de>
2022-06-01 16:19:22 +0200

    Added a folder for reproducing initial conditions of pictures

---
+++ reproduce_pictures/picture_creation.py
@@ -0,0 +1,6 @@
+from ..main import main
+
+if __name__=="__main__":
+    # photonen sphäre 
+    angles = [pi/2, pi/2, 78.245 / 360 * 2 * pi, 78.246 / 360 * 2 * pi]
+    main(1, 50, 1, 2500, 50, .05, angles)
