5e3994004dbce48c2a071826c248d89af6239a56
Altin <altin.shala@web.de>
2022-06-10 21:24:02 +0200

    implemented options to plot in equatorial or meridional plane

--- main.py
+++ main.py
@@ -6,18 +6,58 @@ from plotting import plot_folder, multi_plot
 from initialconditions import lin_comb, eq_plane_grid, angle_calc, shadow_angle_calc
 
 
-def main(mass, distance, quad_param, steps, grid_steps, delta, angles=None, forward_backward="backward"):
+def example_meridional(mass, distance, quad_param, steps, grid_steps, delta,
+                       angles=None, forward_backward="backward"):
+    """
+    Plot meridional plane example picture
+    """
+    lower_theta_angle, higher_theta_angle = [60, 90]
     start = time.time()
     ort = [0, distance, pi / 2, pi]  # t r theta phi
-    lower_phi_angle, higher_phi_angle, angle_steps = angle_calc(mass, quad_param, distance)
     if angles is None:
-        angles = [pi / 2, pi / 2,
-                  lower_phi_angle * 2 * pi / 360, (lower_phi_angle + grid_steps * angle_steps) * 2 * pi / 360]
+        angles = [lower_theta_angle * pi / 180, higher_theta_angle * pi / 180,
+                  pi / 2, pi / 2]
     geschw, asoc_points_on_square = lin_comb(ort, quad_param, mass, angles, grid_steps)
     seed_folder = init_calculation(mass, quad_param, ort, geschw[0])
     light_or_dark = []
     doomtimer = 0
+    upper_angle = lower_theta_angle
+    for count, gesch in enumerate(geschw):
+        print(f"{count} out of {len(geschw)}")
+        move_flag = data_make(ort, gesch, seed_folder, [quad_param, mass], steps, forward_backward, delta)
+        light_or_dark.append(move_flag)
+        upper_angle += (higher_theta_angle-lower_theta_angle) / grid_steps
+        if move_flag == 0:
+            doomtimer += 1
+        if doomtimer == 2:
+            break
+    shadow_angle = shadow_angle_calc(lower_theta_angle, light_or_dark,
+                                     (higher_theta_angle-lower_theta_angle) / grid_steps)
+
+    plot_folder(seed_folder, angles, ort, [quad_param, mass], distance,
+                show_or_not=True, print_black_geodesics=True, d_plot=False, zoom_plot=True, plane="meridional")
+    plot_shadow(asoc_points_on_square, light_or_dark, seed_folder)
+    print(time.time() - start)
+
+
+def example_equatorial(mass, distance, quad_param, steps, grid_steps, delta, angles=None, forward_backward="backward"):
+    """
+    Plot equatorial plane example picture
+    """
+    start = time.time()
+    ort = [0, distance, pi / 2, pi]  # t r theta phi
+    lower_phi_angle, higher_phi_angle, angle_steps = angle_calc(mass, quad_param, distance)     # close to the shadow
+    if angles is None:
+        angles = [pi / 2, pi / 2,
+                  lower_phi_angle * 2 * pi / 360, higher_phi_angle * 2 * pi / 360]
+    geschw, asoc_points_on_square = lin_comb(ort, quad_param, mass, angles, grid_steps)
+    geschw_2, asoc_points_on_square = lin_comb(ort, quad_param, mass, [pi / 2, pi / 2,
+                  60 * 2 * pi / 360, 90 * 2 * pi / 360], 10)
+    seed_folder = init_calculation(mass, quad_param, ort, geschw[0])
+    light_or_dark = []
+    doomtimer = 0
     upper_angle = lower_phi_angle
+
     for count, gesch in enumerate(geschw):
         print(f"{count} out of {len(geschw)}")
         move_flag = data_make(ort, gesch, seed_folder, [quad_param, mass], steps, forward_backward, delta)
@@ -29,13 +69,20 @@ def main(mass, distance, quad_param, steps, grid_steps, delta, angles=None, forw
             break
     shadow_angle = shadow_angle_calc(lower_phi_angle, light_or_dark, angle_steps)
 
+    for count, gesch in enumerate(geschw_2):
+        print(f"visual geodesics {count} out of {len(geschw_2)}")
+        move_flag = data_make(ort, gesch, seed_folder, [quad_param, mass], steps, forward_backward, delta)
+
     plot_folder(seed_folder, angles, ort, [quad_param, mass], distance,
-                show_or_not=True, print_black_geodesics=True, d_plot=False, zoom_plot=True)
+                show_or_not=True, print_black_geodesics=True, d_plot=False, zoom_plot=True, plane="equatorial")
     plot_shadow(asoc_points_on_square, light_or_dark, seed_folder)
     print(time.time() - start)
 
 
 def q_compare(mass, distance, quad_list, steps, grid_steps, delta, angles=None, forward_backward="backward"):
+    """
+    plot example q- value comparison in the equatorial plane
+    """
     start = time.time()
     ort = [0, distance, pi / 2, pi]  # t r theta phi
     parent_folders = []
@@ -68,15 +115,16 @@ def q_compare(mass, distance, quad_list, steps, grid_steps, delta, angles=None, 
 
 
 # TODO 1: . Teilchen fliegen sehr weit weg manchmal -> gtt exponential wachstum
-#       untersuchen um diese auszusortieren  - fixed durch gut gewähltes koordinatensystem
+#       untersuchen um diese auszusortieren  - fixed durch gut gewähltes koordinatensystem - doch nicht fixed, ist ein
+#       quirk des chaotischen verhaltens
 # TODO 2:   koordinaten der initialen geschwindigkigkeitsvektoren fixen - fixed durch lokal minkowski wahl
 # TODO 3: irgendwo ist ein mixup zwischen theta und phi Koordinaten bei den inital conditions -- fixed
 # TODO 4: REPULSIVE GRAVITY BEI SCHWARZSCHILD ? GIBBET NICHT WAS IST HIER FALSCH
-#  - fixed RK4 war SCHULD wieso ? Gute Frage
+#  - fixed RK4 war SCHULD wieso ? Gute Frage - sollte neuen integrator schreiben weil euler zu langsam
 # TODO 5: DREHER IM KOORDINATEN SYSTEM: FUNKTIONIERT BISHER NUR RICHTIG WENN AX UND AY VERTAUSCHT SIND
 
 
 if __name__ == '__main__':
     # main(1, 50, 1, 600, 11, .5)   # angles = [pi/2, pi/2 , 120/360 * pi, 240/360 * pi]  # example
-    # main(1, 50, -0.3, 25000, 100, .005)
-    q_compare(1, 50, [1, 0.5, 0, -0.25], 100000, 30, .001)
+    example_meridional(1, 50, 1, 25000, 50, .005)
+    # q_compare(1, 50, [1, 0.5, 0, -0.25], 100000, 30, .001)
--- plotting/plots.py
+++ plotting/plots.py
@@ -15,6 +15,7 @@ class FolderClass(QMainWindow):
     """
     printing a folder class
     """
+
     def __init__(self):
         super(FolderClass, self).__init__()
         self.setWindowTitle('Anwendung')
@@ -39,7 +40,7 @@ class FolderClass(QMainWindow):
         ax.set_zlim([-r_anzeige, r_anzeige])
 
         for elements in csv_names:
-            filename = os.path.join(csv_loc,elements)
+            filename = os.path.join(csv_loc, elements)
             df = pd.read_csv(filename)
             df["xx"] = df["rr"] * cos(df["phi"]) * sin(df["theta"])
             df["yy"] = df["rr"] * sin(df["phi"]) * sin(df["theta"])
@@ -57,93 +58,123 @@ def window():
     sys.exit(app.exec_())
 
 
-def plot_folder(seed_folder, angles, ort, params, dist, show_or_not, print_black_geodesics, d_plot, zoom_plot):
-    csv_loc = seed_folder + "/csvs_full/"
-    shadow_loc = seed_folder + "/csvs_aborted/"
-    csv_names = os.listdir(csv_loc)
-    shadow_csv = os.listdir(shadow_loc)
-    quad_param_d, mass_d = params
-    phi_low = around(angles[2] * (360 /(2*pi)), decimals=4)
-    phi_up = around(angles[3] * (360 /(2*pi)), decimals=4)
-    the_low = around(angles[0] * (180 /(pi)), decimals=4)
-    the_up = around(angles[1] * (180 /(pi)), decimals=4)
-    r_anzeige = 1.1 * dist
-    if d_plot:
-        ax = plt.axes(projection='3d')
-        ax.set_xlim([-r_anzeige, r_anzeige])
-        ax.set_ylim([-r_anzeige, r_anzeige])
-        ax.set_zlim([-r_anzeige, r_anzeige])
-        ax.set_xlabel("c=G=M=1")
+def three_d_plotter(r_anzeige, csv_names, csv_loc, print_black_geodesics, shadow_csv, shadow_loc):
+    ax = plt.axes(projection='3d')
+    ax.set_xlim([-r_anzeige, r_anzeige])
+    ax.set_ylim([-r_anzeige, r_anzeige])
+    ax.set_zlim([-r_anzeige, r_anzeige])
+    ax.set_xlabel("c=G=M=1")
 
-        for elements in csv_names:
-            filename = os.path.join(csv_loc,elements)
-            df = pd.read_csv(filename)
-            rr = df.loc[0,:]
-            phi = df.loc[2,:]
-            theta = df.loc[1,:]
-            xx = rr * cos(phi) * sin(theta)
-            yy = rr * sin(phi) * sin(theta)
-            zz = rr * cos(theta)
-            ax.plot3D(xx, yy, zz, "b-")
-        if print_black_geodesics:
-            for elements in shadow_csv:
-                filename = os.path.join(shadow_loc, elements)
-                df = pd.read_csv(filename)
-                rr = df.loc[0, :]
-                phi = df.loc[2, :]
-                theta = df.loc[1, :]
-                xx = rr * cos(phi) * sin(theta)
-                yy = rr * sin(phi) * sin(theta)
-                zz = rr * cos(theta)
-                ax.plot3D(xx, yy, zz, "k-")
-        ax.legend()
-        sph_make(ax)
-    else:
-        ax = plt.axes()
-        ax.set_title(f"r = {dist} M= {mass_d} q= {quad_param_d} ϕ = [{phi_low}, {phi_up}] θ = [{the_low}, {the_up}]" )
-        ax.set_xlim([-r_anzeige, r_anzeige])
-        ax.set_ylim([-r_anzeige, r_anzeige])
-        axins = ax.inset_axes([0.7, 0.5, 0.25, 0.25])
-        x1, x2, y1, y2 = -.1, .1, 2*quad_param_d + 3*mass_d - 0.1, 2*quad_param_d + 3*mass_d + 0.1
-        if zoom_plot:
-            axins.set_xlim(x1, x2)
-            axins.set_ylim(y1, y2)
-            ax.indicate_inset_zoom(axins, edgecolor="black")
-
-        for elements in csv_names:
-            filename = os.path.join(csv_loc, elements)
+    for elements in csv_names:
+        filename = os.path.join(csv_loc, elements)
+        df = pd.read_csv(filename)
+        rr = df.loc[0, :]
+        phi = df.loc[2, :]
+        theta = df.loc[1, :]
+        xx = rr * cos(phi) * sin(theta)
+        yy = rr * sin(phi) * sin(theta)
+        zz = rr * cos(theta)
+        ax.plot3D(xx, yy, zz, "b-")
+    if print_black_geodesics:
+        for elements in shadow_csv:
+            filename = os.path.join(shadow_loc, elements)
             df = pd.read_csv(filename)
             rr = df.loc[0, :]
             phi = df.loc[2, :]
             theta = df.loc[1, :]
             xx = rr * cos(phi) * sin(theta)
             yy = rr * sin(phi) * sin(theta)
-            ax.plot(xx, yy, "b-", label="Light")
+            zz = rr * cos(theta)
+            ax.plot3D(xx, yy, zz, "k-")
+    ax.legend()
+    sph_make(ax)
+
+
+def plane_chooser(xx, yy, zz, plane="equatorial"):
+    if plane == "equatorial":
+        pl_ax_1 = xx
+        pl_ax_2 = yy
+    elif plane == "meridional":
+        pl_ax_1 = xx
+        pl_ax_2 = zz
+    else:
+        pl_ax_1 = xx
+        pl_ax_2 = yy
+    return pl_ax_1, pl_ax_2
+
+
+def two_d_plotter(dist, mass_d, quad_param_d, phi_low, phi_up, r_anzeige, zoom_plot, csv_names, csv_loc,
+                  print_black_geodesics, shadow_csv, shadow_loc, the_low, the_up, seed_folder, show_or_not,
+                  plane="equatorial"):
+    if plane == "meridional":
+        zoom_plot = False
+    ax = plt.axes()
+    ax.set_title(f"r = {dist} M= {mass_d} q= {quad_param_d} ϕ = [{phi_low}, {phi_up}] θ = [{the_low}, {the_up}]")
+    ax.set_xlim([-r_anzeige, r_anzeige])
+    ax.set_ylim([-r_anzeige, r_anzeige])
+    if zoom_plot:
+        axins = ax.inset_axes([0.7, 0.5, 0.25, 0.25])
+    x1, x2, y1, y2 = -.1, .1, 2 * quad_param_d + 3 * mass_d - 0.1, 2 * quad_param_d + 3 * mass_d + 0.1
+    if zoom_plot:
+        axins.set_xlim(x1, x2)
+        axins.set_ylim(y1, y2)
+        ax.indicate_inset_zoom(axins, edgecolor="black")
+    for elements in csv_names:
+        filename = os.path.join(csv_loc, elements)
+        df = pd.read_csv(filename)
+        rr = df.loc[0, :]
+        phi = df.loc[2, :]
+        theta = df.loc[1, :]
+        xx = rr * cos(phi) * sin(theta)
+        yy = rr * sin(phi) * sin(theta)
+        zz = rr * cos(theta)
+        pl_ax_1, pl_ax_2 = plane_chooser(xx, yy, zz, plane)
+        ax.plot(pl_ax_1, pl_ax_2, "b-", label="Light")
+        if zoom_plot:
+            axins.plot(pl_ax_1, pl_ax_2, "b-", label="Light")
+    if print_black_geodesics:
+        for elements in shadow_csv:
+            filename = os.path.join(shadow_loc, elements)
+            df = pd.read_csv(filename)
+            rr = df.loc[0, :]
+            phi = df.loc[2, :]
+            theta = df.loc[1, :]
+            xx = rr * cos(phi) * sin(theta)
+            yy = rr * sin(phi) * sin(theta)
+            zz = rr * cos(theta)
+            pl_ax_1, pl_ax_2 = plane_chooser(xx, yy, zz, plane)
+            ax.plot(pl_ax_1, pl_ax_2, "k-", label="Shadow")
             if zoom_plot:
-                axins.plot(xx, yy, "b-", label="Light")
-        if print_black_geodesics:
-            for elements in shadow_csv:
-                filename = os.path.join(shadow_loc, elements)
-                df = pd.read_csv(filename)
-                rr = df.loc[0, :]
-                phi = df.loc[2, :]
-                theta = df.loc[1, :]
-                xx = rr * cos(phi) * sin(theta)
-                yy = rr * sin(phi) * sin(theta)
-                ax.plot(xx, yy, "k-", label="Shadow")
-                if zoom_plot:
-                    axins.plot(xx, yy, "k-", label="Shadow")
-        black_patch = mpatches.Patch(color='black', label='Shadow geodesics')
-        blue_patch = mpatches.Patch(color='blue', label='Light geodesics')
-        ax.set_xlabel("c=G=M=1")
-
-        ax.legend(handles=[blue_patch,black_patch])
-
+                axins.plot(pl_ax_1, pl_ax_2, "k-", label="Shadow")
+    black_patch = mpatches.Patch(color='black', label='Shadow geodesics')
+    blue_patch = mpatches.Patch(color='blue', label='Light geodesics')
+    ax.set_xlabel("c=G=M=1")
+    ax.legend(handles=[blue_patch, black_patch])
     print(seed_folder + "/figs/" + f"dist_{dist}_complete.png")
     plt.savefig(seed_folder + "/figs/" + f"dist_{dist}_complete.png")
     if show_or_not:
         plt.show()
     plt.close()
+
+
+def plot_folder(seed_folder, angles, ort, params, dist, show_or_not, print_black_geodesics, d_plot, zoom_plot,
+                plane="equatorial"):
+    csv_loc = seed_folder + "/csvs_full/"
+    shadow_loc = seed_folder + "/csvs_aborted/"
+    csv_names = os.listdir(csv_loc)
+    shadow_csv = os.listdir(shadow_loc)
+    quad_param_d, mass_d = params
+    phi_low = around(angles[2] * (360 / (2 * pi)), decimals=4)
+    phi_up = around(angles[3] * (360 / (2 * pi)), decimals=4)
+    the_low = around(angles[0] * (180 / (pi)), decimals=4)
+    the_up = around(angles[1] * (180 / (pi)), decimals=4)
+    r_anzeige = 1.1 * dist
+    if d_plot:
+        three_d_plotter(r_anzeige, csv_names, csv_loc, print_black_geodesics, shadow_csv, shadow_loc)
+    else:
+        two_d_plotter(dist, mass_d, quad_param_d, phi_low, phi_up, r_anzeige, zoom_plot, csv_names, csv_loc,
+                      print_black_geodesics, shadow_csv, shadow_loc, the_low, the_up, seed_folder, show_or_not,
+                      plane)
 
 
 def subfolders(parent_folder):
@@ -154,13 +185,13 @@ def subfolders(parent_folder):
     return shadow_csv, csv_names, shadow_loc, csv_loc
 
 
-def simpler_plot(ax, parent_folder, quad, shadow_angle):
+def part_plot(ax, parent_folder, quad, shadow_angle):
     shadow_csv, csv_names, shadow_loc, csv_loc = subfolders(parent_folder)
     mass = 1
     r_anzeige = 50
     ax.set_xlim([-r_anzeige, r_anzeige])
     ax.set_ylim([-r_anzeige, r_anzeige])
-    ax.set_title(f"q = {quad}, α = {around(shadow_angle,3)}")
+    ax.set_title(f"q = {quad}, α = {around(shadow_angle, 3)}")
     axins = ax.inset_axes([0.65, 0.65, 0.25, 0.25])
     x1, x2, y1, y2 = -.1, .1, 2 * quad + 3 * mass - 0.1, 2 * quad + 3 * mass + 0.1
     axins.set_xlim(x1, x2)
@@ -193,7 +224,7 @@ def multi_plot(parent_folders, quad_list, shadow_list):
     fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(18, 5))
     axis_objects = [ax[0, 0], ax[0, 1], ax[1, 0], ax[1, 1]]
     for num, parent_folder in enumerate(parent_folders):
-        simpler_plot(axis_objects[num], parent_folder, quad_list[num], shadow_list[num])
+        part_plot(axis_objects[num], parent_folder, quad_list[num], shadow_list[num])
     plt.tight_layout()
     plt.savefig(parent_folders[-1] + "/figs/" + f"q_compare.png")
 
